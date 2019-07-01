52d82d0ed334fd52f02bbe8ad10210d29fe22f60
Mia Szarvas <mia.szarvas@gmail.com>
2019-07-01 16:34:13 +0300

    add reducers and actions

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,14 @@
+
 import React from 'react';
 
-const cities = [
-  { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
-  { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
-  { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
-];
+import CityList from '../containers/city_list';
+import ActiveCity from '../containers/active_city';
 
 const App = () => {
   return (
     <div className="app">
-      <CityList cities={cities} />
+      <CityList />
+      <ActiveCity />
     </div>
   );
 };

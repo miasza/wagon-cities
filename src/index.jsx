56a77240788a82a9eb750f33ac97b343bfe9d618
Mia Szarvas <mia.szarvas@gmail.com>
2019-07-01 16:40:42 +0300

    combine reducers

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 // internal modules
-import reducers from 'src/reducers';
+import activeCityReducer from './reducers/active_city_reducer';
+import citiesReducer from './reducers/cities_reducer';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
+
+const reducers = combineReducers({
+  cities: citiesReducer,
+  activeCity: activeCityReducer
+});
 
 // render an instance of the component in the DOM
 ReactDOM.render(
